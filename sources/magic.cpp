31fad46293de5b022cc8e61113fe552b57e2274c
SovesT1337 <temirkhan2002@gmail.com>
2021-10-13 11:26:33 +0300

    VI Commit

--- sources/magic.cpp
+++ sources/magic.cpp
@@ -5,39 +5,32 @@
 #include <experiment.hpp>
 
 void print(size_t number, size_t size, size_t mode) {
-  cout << "  - experiment:" << endl;
-  cout << "    number: " << number << endl;
-  cout << "    input_data: " << endl;
-  cout << "      buffer_size: " << size << " mb" << endl;
-  cout << "    results: " << endl;
-  if (mode == 1)
-    cout << "      duration: "
-         << experiment_forward(size * 1024 * 1024 * 8 / 32) / 1000 << endl;
-  if (mode == 2)
-    cout << "      duration: "
-         << experiment_backward(size * 1024 * 1024 * 8 / 32) / 1000 << endl;
-  if (mode == 3)
-    cout << "      duration: "
-         << experiment_random(size * 1024 * 1024 * 8 / 32) / 1000 << endl;
+  int MbinKb = 1024 * 1024 * 8 / 32;
+  cout << "  - experiment:" << endl
+       << "    number: " << number << endl
+       << "    input_data: " << endl
+       << "      buffer_size: " << size << " mb" << endl
+       << "    results: " << endl
+       << "      duration: ";
+  if (mode == 1) cout << experiment_forward(size * MbinKb) / 1000 << endl;
+  if (mode == 2) cout << experiment_backward(size * MbinKb) / 1000 << endl;
+  if (mode == 3) cout << experiment_random(size * MbinKb) / 1000 << endl;
 }
 
 void magic() {
-  cout << "investigation: " << endl;
-  cout << "  travel variant: forward" << endl;
+  cout << "investigation: " << endl << "  travel variant: forward" << endl;
   print(1, 1, 1);
   print(2, 2, 1);
   print(3, 4, 1);
   print(4, 8, 1);
   print(5, 12, 1);
-  cout << "investigation: " << endl;
-  cout << "  travel variant: backward" << endl;
+  cout << "investigation: " << endl << "  travel variant: backward" << endl;
   print(6, 1, 2);
   print(7, 2, 2);
   print(8, 4, 2);
   print(9, 8, 2);
   print(10, 12, 2);
-  cout << "investigation: " << endl;
-  cout << "  travel variant: random" << endl;
+  cout << "investigation: " << endl << "  travel variant: random" << endl;
   print(11, 1, 3);
   print(12, 2, 3);
   print(13, 4, 3);
